6e08866ba28d72be1247b1490c866fe39db2d342
mateusvvv <mat3usvvv619@gmail.com>
2026-06-03 00:15:54 -0300

    correcoes

--- script.js
+++ script.js
@@ -1722,6 +1722,13 @@ async function finalizarVistoria(category) {
         }
     }
 
+    // Aciona a animação de check no botão
+    const btnSubmit = document.querySelector(`#${category} .btn-submit`);
+    if (btnSubmit) {
+        btnSubmit.classList.add('success-anim');
+        setTimeout(() => btnSubmit.classList.remove('success-anim'), 2000);
+    }
+
     state.dadosTemporariosVistoria = {
         viaturaId: state.selectedViatura,
         tabletId: category === "tablets" ? state.selectedViatura : null,
